2bdc201ec156709c19acbf8bcbea21acd4e56c11
binarykevz <107824678+binarykevz@users.noreply.github.com>
2026-07-20 16:23:17 +0800

    Update commandRunner.ts

--- src/commandRunner.ts
+++ src/commandRunner.ts
@@ -1,31 +1,27 @@
 import { Context } from "grammy";
-import * as mainDb from "./db";
-import * as cmdsDb from "./commandsDb";
+import * as db from "./db";
 
 export interface CommandAPI {
     ctx: Context;
     db: {
-        getConfig: typeof mainDb.getConfig;
-        setConfig: typeof mainDb.setConfig;
-        deleteConfig: typeof mainDb.deleteConfig;
-        getAllConfig: typeof mainDb.getAllConfig;
-        addAllowedUser: typeof mainDb.addAllowedUser;
-        removeAllowedUser: typeof mainDb.removeAllowedUser;
-        listAllowedUsers: typeof mainDb.listAllowedUsers;
-        getCommand: typeof cmdsDb.getCommand;
-        saveCommand: typeof cmdsDb.saveCommand;
-        deleteCommand: typeof cmdsDb.deleteCommand;
-        listCommands: typeof cmdsDb.listCommands;
+        getConfig: typeof db.getConfig;
+        setConfig: typeof db.setConfig;
+        deleteConfig: typeof db.deleteConfig;
+        getAllConfig: typeof db.getAllConfig;
+        addAllowedUser: typeof db.addAllowedUser;
+        removeAllowedUser: typeof db.removeAllowedUser;
+        listAllowedUsers: typeof db.listAllowedUsers;
+        getCommand: typeof db.getCommand;
+        saveCommand: typeof db.saveCommand;
+        deleteCommand: typeof db.deleteCommand;
+        listCommands: typeof db.listCommands;
     };
     auth: {
-        isOwner: typeof mainDb.isOwner;
-        isAllowedUser: typeof mainDb.isAllowedUser;
-        getOwner: typeof mainDb.getOwner;
+        isOwner: typeof db.isOwner;
+        isAllowedUser: typeof db.isAllowedUser;
+        getOwner: typeof db.getOwner;
     };
-    utils: {
-        fetch: typeof fetch;
-        Bun: typeof Bun;
-    };
+    utils: { fetch: typeof fetch; Bun: typeof Bun };
 }
 
 export async function executeCommandCode(code: string, ctx: Context): Promise<void> {
@@ -33,30 +29,18 @@ export async function executeCommandCode(code: string, ctx: Context): Promise<vo
     try {
         const transpiler = new Bun.Transpiler({ loader: "ts" });
         jsCode = transpiler.transformSync(code);
-    } catch {
-        jsCode = code;
-    }
+    } catch { jsCode = code; }
 
     const api: CommandAPI = {
         ctx,
         db: {
-            getConfig: mainDb.getConfig,
-            setConfig: mainDb.setConfig,
-            deleteConfig: mainDb.deleteConfig,
-            getAllConfig: mainDb.getAllConfig,
-            addAllowedUser: mainDb.addAllowedUser,
-            removeAllowedUser: mainDb.removeAllowedUser,
-            listAllowedUsers: mainDb.listAllowedUsers,
-            getCommand: cmdsDb.getCommand,
-            saveCommand: cmdsDb.saveCommand,
-            deleteCommand: cmdsDb.deleteCommand,
-            listCommands: cmdsDb.listCommands,
+            getConfig: db.getConfig, setConfig: db.setConfig, deleteConfig: db.deleteConfig,
+            getAllConfig: db.getAllConfig, addAllowedUser: db.addAllowedUser,
+            removeAllowedUser: db.removeAllowedUser, listAllowedUsers: db.listAllowedUsers,
+            getCommand: db.getCommand, saveCommand: db.saveCommand,
+            deleteCommand: db.deleteCommand, listCommands: db.listCommands,
         },
-        auth: {
-            isOwner: mainDb.isOwner,
-            isAllowedUser: mainDb.isAllowedUser,
-            getOwner: mainDb.getOwner,
-        },
+        auth: { isOwner: db.isOwner, isAllowedUser: db.isAllowedUser, getOwner: db.getOwner },
         utils: { fetch, Bun },
     };
 
